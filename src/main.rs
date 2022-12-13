ffec02115ce48ad4bc4317ba098139dd1194bcb7
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2022-12-13 08:47:01 +0100

    feat: add spinner while awaiting response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 use arandu::get_ai_completion;
 use clap::Parser;
+use spinners::{Spinner, Spinners};
+use std::thread::sleep;
+use std::time::Duration;
 
 /// Arandu CLI - Get code suggestions using OpenAI models
 #[derive(Parser, Debug)]
@@ -13,12 +16,19 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
+    let mut sp = Spinner::new(
+        Spinners::Dots12,
+        "The AI 🤖 is working on a response...".into(),
+    );
+
     let suggestion_response = get_ai_completion(args.prompt)?;
     let suggestion_response_text = &suggestion_response
         .choices
         .get(0)
         .expect("Failed while indexing suggestion response")
         .text;
+
+    sp.stop_with_newline();
     println!("The response is: {suggestion_response_text:?}");
     Ok(())
 }
